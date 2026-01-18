79d9a31d16b49817bb7d317e99d597e0b49454cb
The Forsaken <forsakenarchive@tuta.io>
2026-01-19 01:07:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,36 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+
+const galleryImages = document.querySelectorAll(".card img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+
+// Open fullscreen
+galleryImages.forEach(img => {
+  img.addEventListener("click", () => {
+    lightbox.classList.add("active");
+    lightboxImg.src = img.src;
+    document.body.style.overflow = "hidden";
+  });
+});
+
+// Close on click outside image
+lightbox.addEventListener("click", () => {
+  lightbox.classList.remove("active");
+  document.body.style.overflow = "auto";
+});
+
+// Close on ESC
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    lightbox.classList.remove("active");
+    document.body.style.overflow = "auto";
+  }
+});
+
+
+document.addEventListener("contextmenu", (e) => {
+  e.preventDefault();
+});
+
